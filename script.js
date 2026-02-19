e862cd5ea3e9a0a21e04293c1229ac7e5fd2062b
valeriosordini <valeriosordini02@gmail.com>
2026-02-19 11:37:43 +0100

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ function draw() {
   let x = 50; // margine orizzontale
   let blockHeight = 80; // altezza dei blocchi
 
-  // Disegna i blocchi di RAM
+  // blocchi RAM
   for (let i = 0; i < ramBlocks.length; i++) {
     if (allocated[i]) {
       fill(100, 200, 100); // verde per allocato
@@ -20,7 +20,29 @@ function draw() {
       fill(200, 200, 200); // grigio per libero
     }
     rect(x, y, 200, blockHeight);
+    
+    // testo a lato con dimensione del blocco
+    fill(0); //colore testo (nero)
+    textAlign(LEFT, CENTER);
+    text(ramBlocks[i] + " MB", x + 210, y + blockHeight / 2);
+
     y += blockHeight + 5; // distanza tra blocchi
   }
+}
+
+function alloca() {
+  let size = int(select('#size').value());
+  //verifica se input è troppo grande
+  
+  // First Fit: trova il primo blocco libero sufficientemente grande
+  let allocato = false; // boolean per verificare se il processo è stato allocato
+  for (let i = 0; i < ramBlocks.length; i++) {
+    if (!allocated[i] && size <= ramBlocks[i] && !allocato) {
+      allocated[i] = true;
+      allocato = true; 
+    }
+  }
+
+  // aggiornamento informazioni sopra
 }
 
